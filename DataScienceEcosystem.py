bd5bc7a349c2390371df824f8cc3c2dd87e7bc98
999Anita999 <168868681+999Anita999@users.noreply.github.com>
2024-05-06 12:55:00 -0500

    Add files via upload
    Added Jupyter Notebook file that was the first homework assignment in Coursera:IBM Data Science Professional.Data Science Tools called DataScienceEcosystem.ipynb

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## Author
+# Anita Gandhi
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Objectives
+# - List popular languages that Data Scientists use
+# - List commonly used libraries used by Data Scientists
+# - Create a single column table
+# - Perform simple calculations
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. plotly
+# 2. ggplot
+# 3. Pandas
+
+# |Data Science Tools|
+# |---|
+# |R Studio|
+# |Jupyter Notebooks|
+# |Jupyter Labs|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+#This is a simple arithmetic expression to multiply then add integers
+
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by dividing by 60
+
+200/60
+
+
+# In[ ]:
+
+
+
+
